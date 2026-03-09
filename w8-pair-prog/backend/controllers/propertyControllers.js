2c650f2e2bee480d204b93961800ca5107201de0
Sourin <frans.rastas@metropolia.fi>
2026-03-09 11:09:36 +0200

    [iter2] feat(properties): implemented GET, DELETE /api/properties/:id to delete and fetch property by ID

--- w8-pair-prog/backend/controllers/propertyControllers.js
+++ w8-pair-prog/backend/controllers/propertyControllers.js
@@ -22,6 +22,43 @@ const createProperty = async (req, res) => {
   res.status(500).json({ message: "Server error" });
 };
 
+// GET /api/properties/:propertyId
+const getPropertyById = async (req, res) => {
+  const { propertyId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(propertyId)) {
+    return res.status(400).json({ message: "Invalid property ID" });
+  }
+  try {
+    const property = await Property.findById(propertyId);
+    if (property) {
+      res.status(200).json(property);
+    } else {
+      res.status(404).json({ message: "Property not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch property" });
+  }
+};
+
+// DELETE /api/properties/:propertyId
+const deleteProperty = async (req, res) => {
+  const { propertyId } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(propertyId)) {
+    return res.status(400).json({ message: "Invalid property ID" });
+  }
+  try {
+    const deleteProperty = await Property.findByIdAndDelete(propertyId);
+    if (deleteProperty) {
+      res.status(204).send();
+    } else {
+      res.status(404).json({ message: "Property not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete property" });
+  }
+};
+
 module.exports = {
   getAllProperties,
   createProperty,
